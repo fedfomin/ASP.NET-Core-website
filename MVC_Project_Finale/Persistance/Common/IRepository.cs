b2f25a1c56678cd3e75bb33d535fe7f5042ed54e
Fede Fomin <wffomin@protonmail.com>
2021-11-16 14:35:05 +0100

    Strato persistenza: inizializzazione della CRUD

---
+++ MVC_Project_Finale/Persistance/Common/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Project_Finale.Persistance.Common
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Get();
+        T Update(T element);
+        T Create(T element);
+        T Delete(T element);
+        T Edit(T element);
+    }
+}
