ec34dbc8e2c82f59b2dd4d2b29ea238c0ba467fb
Fede Fomin <wffomin@protonmail.com>
2021-11-16 14:11:53 +0100

    Add project files.

---
+++ MVC_Project_Finale/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Project_Finale.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
